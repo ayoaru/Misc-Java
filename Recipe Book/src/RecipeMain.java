b559e23d3619c20a59a1cbb8bb558e9a7bc3e30c
Ayomide Arulogun <112424267+ayoaru@users.noreply.github.com>
2023-03-12 23:02:28 -0400

    Created to run w/o GUI

---
+++ Recipe Book/src/RecipeMain.java
@@ -0,0 +1,14 @@
+/*
+ * RecipeMain class
+ * <p>
+ * Class is for running the main functions of this program w/o the terminal.
+ *
+ * @author Shola Arulogun
+ *
+ * @version March 12, 2022
+ */
+
+public class RecipeMain {
+
+
+}
